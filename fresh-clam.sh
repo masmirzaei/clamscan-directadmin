d081665892cde270469e53ee64684d3b7d0c12eb
Masoud Mirzaei <Mirzaei@parspack.local>
2021-03-14 15:21:27 +0330

    update script

--- fresh-clam.sh
+++ fresh-clam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR="/var/lib/clamav"
-DIR2="/usr/local/share"
+DIR2="/usr/local/share/clamav"
 URL=""
 
 if [ -d "$DIR" ]; then
@@ -11,8 +11,8 @@ echo "The path is: " $DIR
 	rm -f $DIR/main.*
 
 	wget -O $DIR/bytecode.cvd $URL/bytecode.cvd
-	wget -O $DIR/daily.cvd $URL/clam/daily.cvd
-	wget -O $DIR/main.cvd $URL/clam/main.cvd
+	wget -O $DIR/daily.cvd $URL/daily.cvd
+	wget -O $DIR/main.cvd $URL/main.cvd
 elif [ -d "$DIR2" ]; then
 echo "The path is: " $DIR2
 	rm -f $DIR2/bytecode.*
@@ -21,8 +21,8 @@ echo "The path is: " $DIR2
 
 
 	wget -O $DIR2/bytecode.cvd $URL/bytecode.cvd
-        wget -O $DIR2/daily.cvd $URL/clam/daily.cvd
-        wget -O $DIR2/main.cvd $URL/clam/main.cvd
+        wget -O $DIR2/daily.cvd $URL/daily.cvd
+        wget -O $DIR2/main.cvd $URL/main.cvd
 
 
 else
